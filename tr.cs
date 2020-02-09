3a515def79009f1116a363c247cdf7fcd5fb8071
RisedSky <risedsky@gmail.com>
2020-02-09 15:07:16 +0100

    Add files via upload

---
+++ tr.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switcher
+{
+    class tr
+    {
+        //NO API, NO,NO
+        public static string misaneIP = "185.255.134.174";
+
+        public static string onminase = "You are playing now on Aluri";
+
+        public static string onoff = "You are currently playing on the official server";
+
+        public static string switchtominase = "Change the server to Aluri";
+
+        public static string switchtooff = "Change the server to the official";
+    }
+}
